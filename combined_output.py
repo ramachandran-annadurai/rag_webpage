64f98f4eefaaff98a175ae5590619a471f56f851
ramachandran-annadurai <ramachandran.annadurai.lm@gmail.com>
2025-06-17 12:34:38 +0530

    Update combined_output.py

--- combined_output.py
+++ combined_output.py
@@ -390,5 +390,7 @@ def combined_predict(input_data: PredictionInput):
 
 # === Run as Script ===
 if __name__ == "__main__":
+    import os
     import uvicorn
-    uvicorn.run("combined_output:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))  # 👈 Fix for Render
+    uvicorn.run("combined_output:app", host="0.0.0.0", port=port, reload=True)
